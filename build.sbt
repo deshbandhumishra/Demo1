a39088d6f2843c396ac65c2acf729e6e12e74798
deshbandhu <deshbandhu.mishra@gmail.com>
2021-03-12 08:29:15 +0530

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,8 @@
+name := "Demo1"
+
+version := "0.1"
+
+scalaVersion := "2.12.12"
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.8" % Test
+libraryDependencies += "com.novocode" % "junit-interface" % "0.11" % Test
+//libraryDependencies += "com.propensive" %% "kaleidoscope-core" % "0.4.0"
---
+++ src/test/scala/MultipleValueSuite.scala
@@ -0,0 +1,14 @@
+//import org.junit.runner.RunWith
+//import org.scalatest.FunSuite
+//import org.scalatestplus.junit.JUnitRunner
+//
+//@RunWith(classOf[JUnitRunner])
+//class MultipleValueSuite extends FunSuite{
+//import FunSets._
+//
+//  test("parseString is implemented"){
+//    assert(parseString("")=="")
+//
+//  }
+//
+//}
